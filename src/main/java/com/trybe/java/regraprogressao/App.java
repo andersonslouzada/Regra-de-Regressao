78382b7ea2094b01cb9220500bbc32fdd90ffe2e
Anderson Louzada <anderson.s.louzada@gmail.com>
2023-11-29 15:34:15 -0300

    fix checkstyle

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -7,11 +7,30 @@ import java.util.Scanner;
  */
 public class App {
 
-  /**
-   * Metodo main.
-   */
-  public static void main(String[] args) {
-    Works works = new Works();
-    works.createWorks();
-  }
+    /**
+     * Metodo main.
+     */
+    public static void main(String[] args) {
+        System.out.println("Digite a quantidade de atividades para cadastrar: ");
+        Scanner scanner = new Scanner(System.in);
+
+        String quantitatyWorks = scanner.nextLine();
+        int quantitaty = Integer.parseInt(quantitatyWorks);
+
+        String[] worksName = new String[quantitaty];
+        int[] worksWeigth = new int[quantitaty];
+
+        int work = 0;
+        while (work < quantitaty) {
+            System.out.println("Digite o nome da atividade " + (work + 1) + ":");
+            String name = scanner.nextLine();
+            worksName[work] = name;
+
+            System.out.println("Digite o peso da atividade " + worksName[work] + ":");
+            String weight = scanner.nextLine();
+            worksWeigth[work] = Integer.parseInt(weight);
+            work += 1;
+        }
+        scanner.close();
+    }
 }
